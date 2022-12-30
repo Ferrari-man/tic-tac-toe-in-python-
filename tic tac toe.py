f3db2a82bf9ac4ae634635180cc9435834b2833c
Ferrari-man <77579548+Ferrari-man@users.noreply.github.com>
2022-12-30 22:13:54 +0200

    Add files via upload

---
+++ tic tac toe.py
@@ -0,0 +1,49 @@
+# Set up the board
+board = [[" ", " ", " "],
+         [" ", " ", " "],
+         [" ", " ", " "]]
+
+# Set up the player's turn
+player = "X"
+
+# Set up the game loop
+game_over = False
+while not game_over:
+    # Display the board
+    for row in board:
+        print(" ".join(row))
+    print()
+
+    # Get the player's move
+    row = None
+    col = None
+    while row is None or col is None:
+        try:
+            row = int(input("Enter row (0-2): "))
+            col = int(input("Enter column (0-2): "))
+        except ValueError:
+            print("Please enter a valid number.")
+    if board[row][col] == " ":
+        board[row][col] = player
+        if player == "X":
+            player = "O"
+        else:
+            player = "X"
+    else:
+        print("That space is already taken!")
+
+    # Check for a win
+    for row in range(3):
+        if board[row][0] == board[row][1] == board[row][2] and board[row][0] != " ":
+            game_over = True
+            print(f"Player {board[row][0]} wins!")
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != " ":
+            game_over = True
+            print(f"Player {board[0][col]} wins!")
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
+        game_over = True
+        print(f"Player {board[0][0]} wins!")
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != " ":
+        game_over = True
+        print(f"Player {board[0][2]} wins!")
